84983cbaffc44344d3241895755c8cc09d3c9066
Khoi Vo <khoi.vo@asnet.com.vn>
2025-12-19 17:25:30 +0700

    chapter 04 function

---
+++ think_python/chapter_04.py
@@ -0,0 +1,17 @@
+import turtle
+
+# Create a turtle screen/window
+screen = turtle.Screen()
+t = turtle.Turtle()
+
+# Draw a square
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+# Keep the window open until clicked
+screen.exitonclick()
